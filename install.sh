8c47964775d31dd405573401c6d2b88e63511232
Enric Lluelles <git@lluell.es>
2020-07-30 01:53:57 +0200

    Remove prefs stuff

--- install.sh
+++ install.sh
@@ -3,7 +3,6 @@ pushd `dirname "${BASH_SOURCE[0]}"` > /dev/null
 CURRENT_DIR=`pwd`
 git submodule update --init
 
-PREF_DIR=$HOME/Library/Preferences
 for f in vimrc gvimrc
 do
   FILEN=$HOME/\.$f
@@ -20,10 +19,4 @@ do
     echo $FILEN
     ln -s $CURRENT_DIR/$f $FILEN
   fi
-done
-for f in preferences/*
-do
-  base_name=${f##*/}
-  rm -rf $PREF_DIR/$base_name
-  cp $f $PREF_DIR/$base_name
 done
