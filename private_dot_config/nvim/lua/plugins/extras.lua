dbf4fb1f1549436f3aa956ee13d6abbc669236ba
Enric Lluelles <git@lluell.es>
2025-03-13 10:31:25 +0100

    Update .config/git/config Add .config/nvim/LICENSE Add .config/nvim/README.md Add .config/nvim/.gitignore Add .config/nvim/.neoconf.json Update .config/nvim/lazy-lock.json Update .config/nvim/lazyvim.json Update .config/nvim/lua/plugins/extras.lua

--- private_dot_config/nvim/lua/plugins/extras.lua
+++ private_dot_config/nvim/lua/plugins/extras.lua
@@ -3,7 +3,5 @@ return {
   { import = "lazyvim.plugins.extras.lang.go" },
   { import = "lazyvim.plugins.extras.lang.clangd" },
   { import = "lazyvim.plugins.extras.editor.fzf" },
-  { import = "lazyvim.plugins.extras.coding.copilot" },
-  { import = "lazyvim.plugins.extras.coding.copilot-chat" },
   { import = "lazyvim.plugins.extras.coding.mini-surround" },
 }
