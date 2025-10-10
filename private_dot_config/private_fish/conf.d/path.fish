d8ea5b955540c1053d249716d4dbee174c0fd44f
Enric Lluelles <git@lluell.es>
2025-10-10 15:13:48 +0200

    more

--- private_dot_config/private_fish/conf.d/path.fish
+++ private_dot_config/private_fish/conf.d/path.fish
@@ -1,7 +1,7 @@
 fish_add_path /usr/local/bin
 fish_add_path /opt/homebrew/opt/node@16/bin
-fish_add_path /$HOME/.asdf/shims
 fish_add_path /$HOME/go/bin
 fish_add_path /$HOME/bin
 fish_add_path /$HOME/.local/bin
 fish_add_path /opt/homebrew/opt/openjdk/bin
+fish_add_path /opt/homebrew/opt/libpq/bin
