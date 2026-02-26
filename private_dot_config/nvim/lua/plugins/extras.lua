d57fca8254a087758ccd75ccc5aec823f4820e0c
Enric Lluelles <git@lluell.es>
2026-02-26 23:41:51 +0100

    more

--- private_dot_config/nvim/lua/plugins/extras.lua
+++ private_dot_config/nvim/lua/plugins/extras.lua
@@ -1,7 +1,30 @@
 return {
+  -- Languages
   { import = "lazyvim.plugins.extras.lang.ruby" },
   { import = "lazyvim.plugins.extras.lang.go" },
   { import = "lazyvim.plugins.extras.lang.clangd" },
+  { import = "lazyvim.plugins.extras.lang.rust" },
+  { import = "lazyvim.plugins.extras.lang.typescript" },
+  { import = "lazyvim.plugins.extras.lang.json" },
+  { import = "lazyvim.plugins.extras.lang.toml" },
+  { import = "lazyvim.plugins.extras.lang.yaml" },
+  { import = "lazyvim.plugins.extras.lang.markdown" },
+
+  -- Editor
   { import = "lazyvim.plugins.extras.editor.fzf" },
+  { import = "lazyvim.plugins.extras.editor.inc-rename" },
+
+  -- Coding
   { import = "lazyvim.plugins.extras.coding.mini-surround" },
+  { import = "lazyvim.plugins.extras.coding.yanky" },
+
+  -- Testing & Debugging
+  { import = "lazyvim.plugins.extras.test.core" },
+  { import = "lazyvim.plugins.extras.dap.core" },
+
+  -- AI
+  { import = "lazyvim.plugins.extras.ai.avante" },
+
+  -- Linting
+  { import = "lazyvim.plugins.extras.linting.eslint" },
 }
--- private_dot_config/nvim/lua/plugins/lsp.lua
+++ private_dot_config/nvim/lua/plugins/lsp.lua
@@ -2,19 +2,32 @@ return {
   {
     "neovim/nvim-lspconfig",
     opts = {
-      autoformat = false,
       servers = {
-        rust_analyzer = { enabled = true },
-        ruby_lsp = { enabled = true },
-        sorbet = { enabled = true },
+        ruby_lsp = {
+          mason = false,
+          root_dir = function(bufnr, on_dir)
+            -- Walk up to find the top-level Gemfile, not a nested component one
+            local fname = vim.api.nvim_buf_get_name(bufnr)
+            local dir = vim.fn.fnamemodify(fname, ":p:h")
+            local root = nil
+            while dir ~= "/" do
+              if vim.uv.fs_stat(dir .. "/Gemfile") then
+                root = dir
+              end
+              dir = vim.fn.fnamemodify(dir, ":h")
+            end
+            if root then
+              on_dir(root)
+            end
+          end,
+        },
+        sorbet = {
+          enabled = true,
+        },
+        clangd = { mason = false },
         rubocop = { enabled = false },
         standardrb = { enabled = false },
-        clangd = { mason = false },
       },
     },
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    enabled = false,
   },
 }
